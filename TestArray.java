9c27135087c3bf3f7feeec816eb1aecdb8f33153
pavan-koneti <pavan.koneti@tigeranalytics.com>
2021-02-10 12:17:01 +0530

    TestArray.java

---
+++ TestArray.java
@@ -0,0 +1,9 @@
+public class TestArray {
+    public static void main(String[] args) {
+        int g[]=new int[4];
+        String[] s={"d","f","d","b"};
+        for (int i=0;i<s.length;i++)
+        System.out.println(s[i]);
+
+    }
+}
