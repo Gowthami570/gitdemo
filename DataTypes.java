95db991af322bd1cd3968353103181581af52d50
pavan-koneti <pavan.koneti@tigeranalytics.com>
2021-02-10 12:21:19 +0530

    DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+
+
+         static String mystring;
+         static int f;
+         static float myfloat;
+         static boolean myboolean;
+         static double mydouble;
+         public static void main(String[] args) {
+
+            //System.out.println("default values");
+            System.out.println(mystring);
+            System.out.println(f);
+            System.out.println("myfloat"+myfloat);
+            System.out.println("myboolean"+myboolean);
+            System.out.println("mydouble"+mydouble);
+         }
+}
