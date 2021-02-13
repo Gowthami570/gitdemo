b0ab64f7e16e8cea4c7aa54d5122c4b4a7d5b4cc
pavan-koneti <pavan.koneti@tigeranalytics.com>
2021-02-13 12:13:09 +0530

    ArrayList1.java

---
+++ ArrayList1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+        public class ArrayList1{
+            public static void main(String args[]){
+                ArrayList<String> list=new ArrayList<>();
+                list.add("Mango");
+                list.add("Apple");
+                list.add("Banana");
+                list.add("Grapes");
+
+                System.out.println(list);
+            }
+        }
+
