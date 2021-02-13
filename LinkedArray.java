38eef4e6924aec32f9817bdd737fdc78c21d3e17
pavan-koneti <pavan.koneti@tigeranalytics.com>
2021-02-13 12:14:37 +0530

    LinkedArray.java

---
+++ LinkedArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+    public class LinkedArray{
+        public static void main(String args[]){
+            List<String> cf=new LinkedList<>();
+            cf.add("smmu");
+            cf.add("gowthu");
+            cf.add("gowthu");
+            cf.add("gowthi");
+            System.out.println(cf);
+        }
+    }
+
+
+
