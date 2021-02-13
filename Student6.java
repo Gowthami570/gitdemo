5d0b675a7fb7b4b1258c76e8001d20e1416b7b18
pavan-koneti <pavan.koneti@tigeranalytics.com>
2021-02-13 12:17:27 +0530

    Student6.java

---
+++ Student6.java
@@ -0,0 +1,16 @@
+
+
+public class Student6{
+    int id;
+    String name;
+
+    void display() {
+        System.out.println(id + " " + name);
+        Student5 S1 = new Student5();
+        Student5 S2 = new Student5();
+        public static void main(String[] args){
+            S1.display();
+            S2.display();
+        }
+    }
+}
