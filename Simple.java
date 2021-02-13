703b9224744725732c79087eead5626455ed6d6f
pavan-koneti <pavan.koneti@tigeranalytics.com>
2021-02-13 12:16:26 +0530

    simple.java

---
+++ Simple.java
@@ -0,0 +1,9 @@
+public class Simple {
+    public static void main(String[] args) {
+         float f=10f;
+         byte b=(byte)f;
+        System.out.println(f);
+        System.out.println(b);
+
+    }
+}
