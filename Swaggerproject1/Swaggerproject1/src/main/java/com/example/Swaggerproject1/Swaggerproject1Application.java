fc712a9aef22e1c1a05f00efd7e30952ee56b75a
pavan-koneti <pavan.koneti@tigeranalytics.com>
2021-02-19 14:36:29 +0530

    project

---
+++ Swaggerproject1/Swaggerproject1/src/main/java/com/example/Swaggerproject1/Swaggerproject1Application.java
@@ -0,0 +1,14 @@
+package com.example.Swaggerproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swaggerproject1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Swaggerproject1Application.class, args);
+	}
+
+}
---
+++ Swaggerproject1/Swaggerproject1/src/test/java/com/example/Swaggerproject1/Swaggerproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Swaggerproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swaggerproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
